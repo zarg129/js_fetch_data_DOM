6f6395e5bf1285db66fc5ca106119fa000dcda31
Dima Diachenko <zarg129@gmail.com>
2020-10-28 14:34:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,66 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+const baseUrl = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const request = (url) => {
+  return fetch(`${baseUrl}${url}`)
+    .then(response => {
+      return response.json();
+    });
+};
+
+const getPhones = () => {
+  const resolver = (resolve, reject) => {
+    resolve(request('/phones.json'));
+
+    setTimeout(() => {
+      reject('Timeout');
+    }, 5000);
+  };
+
+  return new Promise(resolver);
+};
+
+const getlist = getPhones();
+
+getlist
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.warn(error);
+  });
+
+const getPhonesDetails = (id) => {
+  const resolver = (resolve, reject) => {
+    resolve(request(`/phones/${id}.json`));
+
+    reject(new Error('No such id'));
+  };
+
+  return new Promise(resolver);
+};
+
+const getPhoneId = getPhonesDetails('motorola-atrix-4g');
+
+getPhoneId
+  .then(
+    result => console.log(result)
+  )
+  .catch(
+    error => console.log(error)
+  );
+
+const phonesWithDetails = getPhones();
+
+phonesWithDetails
+  .then(
+    list => Promise.all(
+      list.map(phoneInfo => {
+        // ????
+      }
+      )
+    )
+  );
